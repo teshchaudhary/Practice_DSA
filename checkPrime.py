3a4d3307f7052d8005c37c62771c71589d949aec
Tesh Chaudhary <77956153+teshchaudhary@users.noreply.github.com>
2023-01-04 18:01:42 +0530

    Add files via upload

---
+++ checkPrime.py
@@ -0,0 +1,40 @@
+# def checkPrime(n):
+#     i = 2
+#     if n == 1:
+#         return False
+    
+#     while (i**i <= n):
+#         if n % i == 0:
+#             return False
+        
+#         i+=1
+        
+#     return True
+
+
+# print(checkPrime(int(input())))
+
+#Best way
+
+def checkPrime(n):
+    if n == 1:
+        return False
+    
+    if n == 2 or n == 3:
+        return True
+
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    
+    i = 5
+
+    while i*i <= n:
+        if n % i == 0 or n % i+2 == 0:
+            return False
+
+        i += 6 # The numbers between will come under checking divisibility by 2 and 3
+    
+    return True
+
+
+print(checkPrime(int(input())))
