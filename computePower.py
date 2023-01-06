60b92d13f0412aad7d6825a9706991e2011dee1a
Tesh Chaudhary <77956153+teshchaudhary@users.noreply.github.com>
2023-01-06 15:57:07 +0530

    Using bitwise operators

--- computePower.py
+++ computePower.py
@@ -16,21 +16,36 @@
 
 # Optimized
 
+# def power(m, n):
+#     res = 1
+
+#     while n > 0:
+#         if n % 2 != 0:  # The bit becomes 1
+#             res *= m
+#             # No need to do anything for 0
+#         m *= m
+#         n //= 2
+
+#     return res
+
+
+# print(power(2, 10))
+
+# when mod is given for bigger powers then
+
+# -> reduce every multiply with the mod k (if mod k is given)
+
 def power(m, n):
     res = 1
 
     while n > 0:
-        if n % 2 != 0:  # The bit becomes 1
+        if n & 1 != 0:  # The bit is one
             res *= m
             # No need to do anything for 0
         m *= m
-        n //= 2
+        n >>= 1
 
     return res
 
 
 print(power(2, 10))
-
-# when mod is given for bigger powers then
-
-# -> reduce every multiply with the mod k (if mod k is given)
