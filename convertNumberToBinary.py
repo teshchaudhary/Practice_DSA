93c3acae452e91c62118b3d35e0a658e6ddc23e4
Tesh Chaudhary <77956153+teshchaudhary@users.noreply.github.com>
2023-01-06 15:57:56 +0530

    Using bitwise operators

--- convertNumberToBinary.py
+++ convertNumberToBinary.py
@@ -1,9 +1,22 @@
+# def DtB(n):
+#     while n>0:
+#         if n%2!=0:
+#             print(1, end = "")
+#         else:
+#             print(0, end = "")
+#         n//=2
+
+# DtB(21)
+
 def DtB(n):
     while n>0:
-        if n%2!=0:
+        if n & 1 != 0:
             print(1, end = "")
         else:
             print(0, end = "")
-        n//=2
+        n = n>>1
 
 DtB(21)
+
+# right shit 1 is divide by 2
+# left shift 1 is multiply by 2
