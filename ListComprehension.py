508485cf2a3c925b8e621814977b88a644cc21aa
Tesh Chaudhary <77956153+teshchaudhary@users.noreply.github.com>
2023-01-07 18:08:56 +0530

    Add files via upload

---
+++ ListComprehension.py
@@ -0,0 +1,25 @@
+def OddandEven(l):
+
+    l1 = [x for x in l if x % 2 == 0]  # List Comprehension
+    l2 = {x for x in l if x % 2 != 0}  # Set Comprehension
+
+    return (l1, l2)
+
+
+print(OddandEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+# Dictionary Comprehension
+
+keys = [101, 103, 102]
+values = ['gfg', 'ide', 'corse']
+
+d = {keys[i]: values[i] for i in range(len(keys))}
+print(d)
+
+dict(zip(keys, values))  # can zip two lists like this too
+
+
+# Exchanging key values
+
+d1 = {v: k for (k, v) in d.items()}
+print(d1)
