d53cb2bb60f0aadeef71d8f4663636eae41bde05
Tesh Chaudhary <77956153+teshchaudhary@users.noreply.github.com>
2023-01-14 19:13:52 +0530

    Both ascending or descending

--- isSorted.py
+++ isSorted.py
@@ -1,9 +1,18 @@
-def isSorted(li):
-    l = len(li)
-    for i in range(1,l):
-        if li[i-1]>li[i]:
-            return False
-    
-    return True
+def isSorted(arr, n):
+    if n == 1:
+        return 1
 
-print(isSorted([1,8,2,3]))
+    if arr[0] > arr[1]:
+        for i in range(2, n):
+            if arr[i-1] < arr[i]:
+                return 0
+
+    elif arr[0] < arr[1]:
+        for i in range(2, n):
+            if arr[i-1] > arr[i]:
+                return 0
+
+    return 1
+
+print(isSorted([1,2,3,4,5,6], 6))
+print(isSorted([6,5,4,3,2,1], 6))
