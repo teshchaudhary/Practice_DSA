473de2972d8fc852d92387adff62fe619bfd363b
Tesh Chaudhary <77956153+teshchaudhary@users.noreply.github.com>
2023-01-14 16:25:56 +0530

    Delete and shift by both element and index value

--- DeleteAndShift.py
+++ DeleteAndShift.py
@@ -7,3 +7,19 @@ def deleteFromArray(arr, n, idx):  # Last element will be 0
 
 
 print(deleteFromArray([1, 2, 3, 4, 5], 5, 3))
+
+# Delete and shift by an element(First Occurence) in list
+def das(arr, ele):
+    i = 0
+    l = len(arr)
+    while i < l:
+        if arr[i] == ele:
+            for j in range(i, l-1):
+                arr[j] = arr[j+1]
+            arr[l-1] = 0
+        i += 1
+
+    return arr
+
+
+print(das([1, 2, 3, 4, 5], 3))
