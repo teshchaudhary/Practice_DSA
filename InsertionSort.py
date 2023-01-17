dce91e23b17c61ca9720f310e17102a6bb6f3fa8
Tesh Chaudhary <77956153+teshchaudhary@users.noreply.github.com>
2023-01-17 22:55:28 +0530

    Add files via upload

---
+++ InsertionSort.py
@@ -0,0 +1,22 @@
+# We split in two lists one is sorted part another is unsorted as we iterate, we increase the sorted part by inserting one element from the unsorted part to the sorted part
+
+# Worst Case O(n^2)
+# Good for small inputs
+# Best Case O(n)
+
+def insertionSort(arr):
+    l = len(arr)
+    for i in range(1, l):
+        ele = arr[i]  # The element of unsorted list
+        j = i-1  # Just the left side of the unsorted element i.e in sorted list
+        # We'll never go inside this loop if the elements are already sorted
+        while j > 0 and ele < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = ele
+
+
+arr = [1, 5, 3, 8, 2]
+insertionSort(arr)
+print(arr)
