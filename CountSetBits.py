7bed2cf12237da1969a98bf25e23a72a9353087e
Tesh Chaudhary <77956153+teshchaudhary@users.noreply.github.com>
2023-03-02 22:47:43 +0530

    Update CountSetBits.py

--- CountSetBits.py
+++ CountSetBits.py
@@ -1,3 +1,4 @@
+# Naive Solution
 def count(n):
     res = 0
     while n!=0:
@@ -6,6 +7,19 @@ def count(n):
         res += n&1
         n >>= 1
 
+    return res
+
+# Brian Kernigam's Algo
+# if n = 40: 101000
+# then (n-1) = 39: 100111
+
+# All the bits after first set bit becomes 1 
+def count(n):
+    res = 0
+    while n:
+        n = n&(n-1)
+        res += 1
+    
     return res
 
 print(count(10))
