7704aefac3ed1a5c3eed59cae1c8a11b333731ec
Tesh Chaudhary <77956153+teshchaudhary@users.noreply.github.com>
2023-03-03 21:57:25 +0530

    Update OnlyOddOccurence.py

--- OnlyOddOccurence.py
+++ OnlyOddOccurence.py
@@ -1,5 +1,19 @@
-def OddOccurece(arr):
+# def OddOccurence(arr):
+#     for i in arr:
+#         c = arr.count(i)
+#         if c%2 != 0:
+#             return i
+
+
+# XOR Properties
+# x^0 = x
+# x^x = 0
+# x^y^z = y^x^z = x^z^y = all other permutations
+
+def OddOccurence(arr):
+    res = 0
     for i in arr:
-        c = arr.count(i)
-        if c%2 != 0:
-            return i
+        res = res^i
+    
+    return res
+print(OddOccurence([1,2,2,3,1])) 
