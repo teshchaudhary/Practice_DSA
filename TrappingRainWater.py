141350c31eefb388272efb266039e18acc976c54
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-03-19 20:18:56 +0530

    Create

---
+++ TrappingRainWater.py
@@ -0,0 +1,19 @@
+# We are given a array of bars and we need to determine maximum amount of water we can hold between the bars
+
+# O(n^2)
+def func(arr):
+    res = 0
+    l = len(arr)
+    for i in range(1, l-1):
+        leftMax = arr[i]
+        for j in range(0,i):
+            leftMax = max(leftMax, arr[j])
+
+        rightMax = arr[j]
+
+        for j in range(i+1, l):
+            rightMax = max(rightMax, arr[j])
+        
+        res = res + (min(leftMax, rightMax) - arr[i])
+    
+    return res
