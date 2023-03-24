3fb32134fe67c73ff12096c8eb7a51b40e52d34d
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-03-24 21:52:05 +0530

    Update DivideWithoutArithemetic.py

--- DivideWithoutArithemetic.py
+++ DivideWithoutArithemetic.py
@@ -19,3 +19,27 @@ def func1(dividend, divisor):
         quotient = -quotient
 
     return quotient
+
+# O(log(dividend))
+
+def divide(dividend, divisor):
+
+    if ((dividend < 0) ^ (divisor < 0)):
+        sign = -1
+    else:
+        sign = 1
+
+    dividend = abs(dividend)
+    divisor = abs(divisor)
+
+    quotient = 0
+    temp = 0
+
+    for i in range(31, -1, -1):
+        if (temp + (divisor << i) <= dividend):
+            temp += divisor << i
+            quotient |= 1 << i
+
+    if sign == -1:
+        quotient = -quotient
+    return quotient
