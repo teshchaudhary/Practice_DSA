a8c2279254b006505885f977aad85b898678e45a
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-03-29 19:52:55 +0530

    Create LongestEvenOddSubaaray.py

---
+++ LongestEvenOddSubaaray.py
@@ -0,0 +1,15 @@
+def func(arr):
+    res = 1
+    for i in range(len(arr)):
+        seq = 1 # Length of sequence being made while condition is True
+        for j in range(i+1, len(arr)):
+            if (arr[j-1] % 2 == 0 and arr[j] % 2 != 0) or \ # Check the proceeding pattern with all possible cases together
+                (arr[j-1] % 2 != 0 and arr[j] % 2 == 0):
+                seq += 1
+            
+            else:
+                break #If pattern is not followed anymore then break and start again with next element
+        
+        res = max(res, seq)
+    
+    return res
