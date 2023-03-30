f794871175b9d0d59fec0a0c73fef61c605dedc0
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-03-30 21:10:34 +0530

    Update LongestEvenOddSubaaray.py

--- LongestEvenOddSubaaray.py
+++ LongestEvenOddSubaaray.py
@@ -1,4 +1,7 @@
-def func(arr):
+# Naive Solution
+# O(n^2) because we are finding the possibility with each subarray possible
+
+def func0(arr):
     res = 1
     for i in range(len(arr)):
         seq = 1  # Length of sequence being made while condition is True
@@ -12,5 +15,24 @@ def func(arr):
                 break  # If pattern is not followed anymore then break and start again with next element
 
         res = max(res, seq)
+
+    return res
+
+
+# Efficient Solution
+# Kadane's Algorithm
+
+def func1(arr):
+    res = 1
+    curr = 1
+
+    for i in range(1, len(arr)):
+        if (arr[i-1] % 2 == 0 and arr[i] % 2 != 0) or \
+                (arr[i-1] % 2 != 0 and arr[i] % 2 == 0):
+            curr += 1
+            res = max(res, curr)
+
+        else:
+            curr = 1
 
     return res
