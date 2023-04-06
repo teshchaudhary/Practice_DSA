0b289a2d0d6e34152e78706af82e59542104f8ac
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-04-06 06:35:03 +0530

    Update PrefixSum.py

--- PrefixSum.py
+++ PrefixSum.py
@@ -13,3 +13,27 @@ def func1(arr, si, ei):
 
 # arr = [2,8,3,9,6,5,4]
 # print(func1(arr,0,6))
+
+# For above solution we will be needing to process O(n) again and again
+# This costs more time
+# So we can preprocess data once for O(n) and then perform queries in O(1)
+
+def preprocess(arr):
+    l = len(arr)
+    res = [None]*l
+    res[0] = arr[0]
+
+    for i in range(1, l):
+        res[i] = res[i-1] + arr[i]
+    
+    return res
+
+def func2(arr,si,ei):
+    if si == 0:
+        return preprocess(arr)[ei]
+    else:
+        return preprocess(arr)[ei] - preprocess(arr)[si-1]
+
+# arr = [2,8,3,9,6,5,4]
+# print(preprocess(arr))
+# print(func2(arr,1,2))
