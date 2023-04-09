34c213e981c781be56dd1b8d2b3e6e64ced081a2
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-04-09 12:43:34 +0530

    Update MaximumAppearingInRanges.py
    Better Time Complexity

--- MaximumAppearingInRanges.py
+++ MaximumAppearingInRanges.py
@@ -2,6 +2,7 @@
 # We need to tell the maximum appearing element within the ranges
 # ranges will be if first element of left array is 2 and first element of right array is 5 then range will be from 2 to 5 both inclusive
 
+# Naive Solution
 # O(N^Max)
 # N is the number of ranges
 # Max is the last element that can appear (in our case it is 100)
@@ -11,5 +12,23 @@ def func1(left, right):
     for i in range(len(left)):
         for j in range(left[i], right[i]+1):
             freq[j] += 1
+
+    return freq.index(max(freq))
+
+# O(N+Max)
+# Making markers
+
+def func2(left, right):
+    freq = [0] * 101
+
+    # Put marker 1 for the starting of range
+    # Put marker -1 for the ending of range
+    for i in range(len(left)):
+        freq[left[i]] += 1
+        freq[right[i]+1] -= 1
+
+    # Now add up all the marker indexes to get the overlapping elements
+    for i in range(1, len(freq)):
+        freq[i] = freq[i-1] + freq[i]
 
     return freq.index(max(freq))
