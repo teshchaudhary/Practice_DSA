447e9814f346c32afda05e6ae8ff7e45b45c2798
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-04-25 22:31:44 +0530

    Update FindPeakElement.py

--- FindPeakElement.py
+++ FindPeakElement.py
@@ -12,8 +12,22 @@ def func0(arr, n) :
 	for i in range(1, n - 1) :
 		if (arr[i] >= arr[i - 1] and arr[i] >= arr[i + 1]) :
 			return arr[i]
+
+# Getting all peak elements by Naive Method
+def func(arr):
+    l = len(arr)
+    li = []
+    if arr[0] >= arr[1]:
+        li.append (arr[0])
+        
+    elif arr[l-1] > arr[l-2]:
+        li.append (arr[l-1])
     
-    
+    for i in range(1,l-1):
+         if arr[i-1] < arr[i] and arr[i]> arr[i+1]:
+             li.append(arr[i]) 
+
+    return li
 # Efficient Solution
 
 def func1(arr, n):
