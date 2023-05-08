9405c792ada3df62d7783aa195e3e594ad0dcdcb
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-05-08 15:53:13 +0530

    Create EquillibriumPoint.py

---
+++ EquillibriumPoint.py
@@ -0,0 +1,21 @@
+# An equillibrium point is when the sum of all the previous elements from a element is equal to the sum of the all next elements
+# O(n^2)
+def func1(arr):
+    l = len(arr)
+    for i in range(l):
+        ls, rs = 0, 0
+
+        for j in range(i):
+            ls += arr[j]
+        
+        for k in range(i+1, len(arr)):
+            rs += arr[k]
+
+        
+        if ls == rs:
+            return True
+    
+    return False
+
+arr = [3,4,8,-9,9,7]
+print(func1(arr))
