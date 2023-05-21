2fd8f9df5ec659642ae2d5623ef7e1a5f733de23
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-05-21 18:12:59 +0530

    Update ImmediateSmaller.py
    Optimization

--- ImmediateSmaller.py
+++ ImmediateSmaller.py
@@ -8,4 +8,12 @@ def immediateSmaller(arr, x):
         return -1
     return res
 
+def immediateSmaller(self,arr,n,x):
+        res = -1
+        for i in range(len(arr)):
+            if arr[i] < x:
+                if res == -1 or res < arr[i]:
+                    res = arr[i]
+        
+        return res
 print(immediateSmaller([1,2,3,4,5,6], 4))
