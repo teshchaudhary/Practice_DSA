e2cdbe3e67ea606847e84bfceaf378c767f174d0
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-05-21 18:10:38 +0530

    Update ImmediateGreater.py
    Optimization

--- ImmediateGreater.py
+++ ImmediateGreater.py
@@ -1,19 +1,12 @@
 def immediateGreater(arr, x):
-    diff = None
-    res = 0
-    for i in arr:
-        if i > x:
-            if diff is None:
-                diff = i-x
-                res = i
-            elif i-x < diff:
-                diff = i-x
-                res = i
+    res = -1
 
-    if res == 0:
-        return -1
-    else:
-        return res
+    for i in range(len(arr)):
+        if arr[i] > x:
+            if res == -1 or res > arr[i]:
+                res = arr[i]
+
+    return res
 
 
 print(immediateGreater([9, 11, 12, 13], 10))
