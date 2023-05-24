903d9419ce54ac69bab41abf2a6dad206c718c95
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-05-24 07:55:11 +0530

    Update RemoveDuplicatesSArray.py

--- RemoveDuplicatesSArray.py
+++ RemoveDuplicatesSArray.py
@@ -1,9 +1,12 @@
 # Remove duplicates from sorted array
+# This will take space complexity as slicing makes a new copy of the array
+# in worst case the space complexity will go O(n) when all elements are unique
 
 def rem(arr):
     res = 1
     for i in range(1,len(arr)):
+        # Everytime we get a distinct element from the POV of res and i we update the values according to index (res)
         if arr[res-1] != arr[i]:
             arr[res] = arr[i]
-            res += 1
+            res += 1 # res is only updates when we find a new value so that we can make the array distinct
     return arr[:res]
