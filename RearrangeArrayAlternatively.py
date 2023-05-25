f4c8e9a1b76c96ecbeb66ee6eb0283cfeb0adb21
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-05-25 22:35:28 +0530

    Update RearrangeArrayAlternatively.py

--- RearrangeArrayAlternatively.py
+++ RearrangeArrayAlternatively.py
@@ -2,19 +2,28 @@
 # second should be min value, third should be second max, fourth should be second min and so on.
 # Modify the original array itself. Do it without using any extra space. You do not have to return anything
 
-def rearrange(arr, n):
+# Two values can be stored in a single value as
+
+# Dividend = Divisor * Quotient + Remainder
+# Divisor = Maximum number in element + 1
+# Quotient = New Value Stored
+# Remainder = Original Value
+
+def rearrange(self, arr, n):
     si, ei = 0, n-1
 
     maxi = arr[ei]+1
 
+    # To store two values in a single value
     for i in range(n):
-        if i % 2 == 0:
+        if i % 2 == 0:  # For even position the element will be the maximum one
             arr[i] += (arr[ei] % maxi)*maxi
             ei -= 1
 
-        else:
+        else:  # For odd position the element will be the minimum one
             arr[i] += (arr[si] % maxi)*maxi
             si += 1
 
+    # This is to arrange the original value
     for i in range(n):
         arr[i] = arr[i]//maxi
