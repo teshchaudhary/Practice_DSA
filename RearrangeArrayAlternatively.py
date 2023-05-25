0d4f5b0fbd4d60ccfcbced48136b5f2cac7c6144
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-05-25 22:35:10 +0530

    Update RearrangeArrayAlternatively.py
    Problem Statement

--- RearrangeArrayAlternatively.py
+++ RearrangeArrayAlternatively.py
@@ -1,3 +1,7 @@
+# Given a sorted array of positive integers rearrange the array elements in a way that first element should be max value,
+# second should be min value, third should be second max, fourth should be second min and so on.
+# Modify the original array itself. Do it without using any extra space. You do not have to return anything
+
 def rearrange(arr, n):
     si, ei = 0, n-1
 
