13bfd144f4cfcc8e0de25203292eb836ac8d0f9d
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-06-13 07:44:52 +0530

    Update BFS_Disconnected_Graphs.py

--- BFS_Disconnected_Graphs.py
+++ BFS_Disconnected_Graphs.py
@@ -17,13 +17,18 @@ def BFS(adj, s, visited):
     print()
 
 def BFSdis(adj):
+    count = 0
     visited = [False] * len(adj)
 
     for i in range(len(adj)):
         if visited[i] == False:
             BFS(adj, i, visited)
+            count += 1
+
+    return count
 
 
 v = 8
 adj = [[1, 2], [0, 2], [0, 1], [4], [3], [6,7], [5],[5]]
-BFSdis(adj)
+print("Graphs")
+print("Connected Components" ,BFSdis(adj))
