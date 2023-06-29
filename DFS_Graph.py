cee40abbc596b6f9452ce9a1e753c09ede371539
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-06-30 03:13:01 +0530

    Update DFS_Graph.py
    Added iterative approach

--- DFS_Graph.py
+++ DFS_Graph.py
@@ -10,6 +10,33 @@ def DFSrec(adj, s, visited):
 def DFS(adj,s):
     visited = [False] * len(adj)
     DFSrec(adj, s, visited)
+    
 
+def df(adj,s):
+    visited = [False]*len(adj)
+
+    stack = []
+    stack.append(s)
+
+    while stack:
+        s = stack.pop()
+
+        # Stack may contain same vertex twice. So
+        # we need to print the popped item only
+        # if it is not visited.
+        if not visited[s]:
+            print(s,end=' ')
+            visited[s] = True
+
+        # Get all adjacent vertices of the popped vertex s
+        # If a adjacent has not been visited, then push it
+        # to the stack.
+        for u in adj[s]:
+            if not visited[u]:
+                stack.append(u)
+
+adj = [[1, 2], [0, 3, 4], [0, 3], [1, 2, 4], [1, 3]]
+
+df(adj,0)
 adj = [[1, 2], [0, 3, 4], [0, 3], [1, 2, 4], [1, 3]]
 DFS(adj,0)
