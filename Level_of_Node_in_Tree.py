8ea6b332687276199baf67f8836734cd190a6d66
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-07-03 18:08:39 +0530

    Create Level_of_Node_in_Tree.py

---
+++ Level_of_Node_in_Tree.py
@@ -0,0 +1,32 @@
+# DP Solution
+
+def fact(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        table = [[0, 0]] * (n + 1)
+        table[0] = [1, 1]
+        table[1] = [1, 1]
+        for i in range(2, n+1):
+            table[i] = [table[i-1][0] * i, (table[i-1][0] * i)+table[i-1][1]]
+        return table
+
+
+def FindLevel(N):
+    table = fact(20)
+
+    if N == 1:
+        return 1
+
+    i = 1
+
+    while table[i][1] < N:
+        i += 1
+
+    if table[i][1] > N and table[i-1][1] > N:
+        return i - 1
+
+    else:
+        return i
