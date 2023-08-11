aa542d8f4f97b3e989ca3eb57891eba0c45478e4
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-08-11 17:46:49 +0530

    Create Coin_Change.py

---
+++ Coin_Change.py
@@ -0,0 +1,10 @@
+def count(self, coins, N, Sum):
+        
+        dp = [0] * (Sum + 1)
+        dp[0] = 1
+
+        for coin in coins:
+            for i in range(coin, Sum + 1):
+                dp[i] += dp[i - coin]
+
+        return dp[Sum]
