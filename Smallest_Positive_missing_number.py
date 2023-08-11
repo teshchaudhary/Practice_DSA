2073a95358963a8ad96d2539d719af50029d2faa
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-08-11 12:46:40 +0530

    Update Smallest_Positive_missing_number.py

--- Smallest_Positive_missing_number.py
+++ Smallest_Positive_missing_number.py
@@ -1,4 +1,6 @@
-def missingNumber(self,arr,n):
+# Extra Space
+
+def missingNumber(arr,n):
       visited = [False]*(n+1)
       
       for i in range(n):
@@ -10,3 +12,34 @@ def missingNumber(self,arr,n):
               return i
       
       return n + 1
+
+# Extra Space
+def solution(A):
+
+    # To know how long should be the check array
+    m = max(A)
+
+    if m < 1:
+        return 1
+    
+    if len(A) == 1:
+        # If it contains only one element
+        return 2 if A[0] == 1 else 1
+    
+    l = [0] * m
+    for i in range(len(A)):
+        if A[i] > 0:
+            if l[A[i] - 1] != 1:
+                # Changing the value status at the index of our list
+                l[A[i] - 1] = 1
+    
+    for i in range(len(l)):
+        # Encountering first 0, i.e, the element with least value
+        if l[i] == 0:
+            return i + 1
+            # In case all values are filled between 1 and m
+    return i + 2
+
+
+arr = [1,2,3,4,5]
+print(solution(arr))
