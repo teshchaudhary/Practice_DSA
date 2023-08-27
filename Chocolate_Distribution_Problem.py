b2fd8cfe37f33896cd0d930ef0d509f1b4d329f6
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-08-27 18:05:52 +0530

    Update Chocolate_Distribution_Problem.py

--- Chocolate_Distribution_Problem.py
+++ Chocolate_Distribution_Problem.py
@@ -1,3 +1,8 @@
+# Distribute the chocolates in a way that the child that gets minimum number of chococlates and the one that gets maximum number of chocolates, the difference between them should be minimum
+
+# m = number of children
+# arr is such that arr[i] represents the number of chocolates in a packet
+
 def findMinDiff(A,N,M):
       A.sort()
       
@@ -10,3 +15,19 @@ def findMinDiff(A,N,M):
           res = min(res, curr_diff)
   
       return res
+
+
+# Another version
+def minDiff(arr,m) :
+    if (m == 0 or len(arr) == 0) :
+        return 0
+    
+    if (len(arr)<m) :
+        return -1
+    
+    arr.sort()
+    res = arr[m-1] - arr[0]
+    for i in range(1, len(arr)- m+1) :
+        res = min(res,abs(arr[i + m-1] - arr[i]))
+    print(res)
+    
