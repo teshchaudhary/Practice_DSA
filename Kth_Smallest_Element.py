f9ad1c17d76461bc4495e95080ca42e885a1b29c
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-08-31 21:02:40 +0530

    Update Kth_Smallest_Element.py
    One more method

--- Kth_Smallest_Element.py
+++ Kth_Smallest_Element.py
@@ -1,3 +1,4 @@
+# Method 1
 import heapq as hq
 def kthSmallest(arr,k):
 
@@ -7,3 +8,29 @@ def kthSmallest(arr,k):
         res = hq.heappop(arr)
 
     return res
+
+# Method 2
+def lomuto(arr, l, r):
+    x = arr[r]
+    i = l
+    for j in range(l, r):
+        if (arr[j] <= x):
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[i], arr[r] = arr[r], arr[i]
+    return i
+
+def kthsmallest(arr, k) :
+    l = 0 
+    r = len(arr) - 1 
+    while l <= r :
+        p = lomuto(arr,l,r) 
+        if p == k-1 :
+            return arr[p] 
+                
+        elif p > k-1 :
+            r = p - 1 
+                
+        else :
+            l = p + 1 
+    return [-1]
