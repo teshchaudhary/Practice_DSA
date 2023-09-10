8b92cfd366c6537b19cb86d87ec5185908a755e6
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-09-10 18:49:21 +0530

    Create Meeting_the_maximum_guests.py

---
+++ Meeting_the_maximum_guests.py
@@ -0,0 +1,21 @@
+#You are given arrival and departure times of the guests, you need to find the time to go to the party so that you can meet maximum people.
+
+def maxguests(arrivals,departures):
+    arrivals.sort()
+    departures.sort()
+
+    res = 0
+    curr = 0
+    i = j = 0
+    while (i < len(arrivals) and j < len(departures)):
+        if arrivals[i] <= departures[j]:
+            curr += 1
+            i += 1
+        
+        else:
+            curr -= 1
+            j += 1
+
+        res = max(res, curr)
+    
+    return res
