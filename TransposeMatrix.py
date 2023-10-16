8d8aa31f881a9684461851ed26137d603b3a9093
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-10-16 18:52:55 +0530

    Update TransposeMatrix.py

--- TransposeMatrix.py
+++ TransposeMatrix.py
@@ -1,3 +1,4 @@
+# Naive
 def transpose(mat):
     m = len(mat)
 
@@ -8,3 +9,22 @@ def transpose(mat):
     
     for i in temp:
         print(i)
+
+# Better
+def Transpose(mat):
+    m = len(mat)
+
+    for i in range(m):
+        for j in range(i+1, m):
+            mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+    
+    for i in mat:
+        print(i)
+
+mat = [[1,2,3,4],
+       [5,6,7,8],
+       [9,10,11,12],
+       [13,14,15,16]]
+
+transpose(mat)
+Transpose(mat)
