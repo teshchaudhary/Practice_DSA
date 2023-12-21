0f3c7cbe18687b6cd4d881962b837eb290a44f27
Tesh Chaudhary <chaudharytesh@gmail.com>
2023-12-21 20:00:05 +0530

    Create Candy.py

---
+++ Candy.py
@@ -0,0 +1,11 @@
+def minCandy(N, ratings):
+    res = [1]*N
+    for i in range(1,N):
+        if ratings[i]>ratings[i-1]:
+            res[i] = res[i-1]+1
+            
+    for i in range(N-2,-1,-1):
+        if ratings[i]>ratings[i+1] and res[i]<=res[i+1]:
+            res[i] = res[i+1]+1
+            
+    return sum(res)
